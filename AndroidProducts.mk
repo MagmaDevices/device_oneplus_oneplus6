be999c41ce697fb496dc3742f11a7d404d59ce75
Adrian <misiakadrian@gmail.com>
2019-11-01 17:39:50 +0100

    magma: initial

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,8 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/device.mk \
-    $(LOCAL_DIR)/omni_oneplus6.mk
+    $(LOCAL_DIR)/magma_oneplus6.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_oneplus6-user \
-    omni_oneplus6-userdebug \
-    omni_oneplus6-eng \
+    magma_oneplus6-user \
+    magma_oneplus6-userdebug \
+    magma_oneplus6-eng \
--- magma_oneplus6.mk
+++ magma_oneplus6.mk
@@ -19,31 +19,20 @@
 # product configuration (apps).
 #
 
-VENDOR_EXCEPTION_PATHS := oneplus \
-    omni \
-    gapps \
-    pixelstyle
+VENDOR_EXCEPTION_PATHS := oneplus 
 
-# Sample: This is where we'd set a backup provider if we had one
-# $(call inherit-product, device/sample/products/backup_overlay.mk)
+# Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Get the prebuilt list of APNs
-$(call inherit-product, vendor/omni/config/gsm.mk)
+DEVICE_PACKAGE_OVERLAYS += device/oneplus/oneplus6/overlay/common
+DEVICE_PACKAGE_OVERLAYS += device/oneplus/oneplus6t/overlay/device
 
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-
-# must be before including omni part
-TARGET_BOOTANIMATION_SIZE := 1080p
-AB_OTA_UPDATER := true
+# Inherit some common Magma stuff.
+$(call inherit-product, vendor/magma/config/common_full_phone.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/oneplus/oneplus6/overlay/common
 DEVICE_PACKAGE_OVERLAYS += device/oneplus/oneplus6/overlay/device
-DEVICE_PACKAGE_OVERLAYS += vendor/omni/overlay/CarrierConfig
-
-# Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
 
 # get the rest of aosp stuff after ours
 $(call inherit-product, $(SRC_TARGET_DIR)/product/mainline_system_arm64.mk)
@@ -51,14 +40,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/mainline_system_arm64.mk)
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/oneplus/oneplus6/device.mk)
 
-ALLOW_MISSING_DEPENDENCIES := true
+#ALLOW_MISSING_DEPENDENCIES := true
 PRODUCT_SHIPPING_API_LEVEL := 27
 
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service
 
 # Discard inherited values and use our own instead.
-PRODUCT_NAME := omni_oneplus6
+PRODUCT_NAME := magma_oneplus6
 PRODUCT_DEVICE := oneplus6
 PRODUCT_BRAND := OnePlus
 PRODUCT_MANUFACTURER := OnePlus
@@ -69,8 +58,7 @@ PRODUCT_SYSTEM_NAME := OnePlus6
 
 VENDOR_RELEASE := 10:QKQ1.190716.003/1910050400:user/release-keys
 BUILD_FINGERPRINT := OnePlus/OnePlus6/OnePlus6:$(VENDOR_RELEASE)
-OMNI_BUILD_FINGERPRINT := OnePlus/OnePlus6/OnePlus6:$(VENDOR_RELEASE)
-OMNI_PRIVATE_BUILD_DESC := "'OnePlus6-user 10 QKQ1.190716.003 1910050400 release-keys'"
+PRIVATE_BUILD_DESC := "'OnePlus6-user 10 QKQ1.190716.003 1910050400 release-keys'"
 
 PLATFORM_SECURITY_PATCH_OVERRIDE := 2019-09-05
 
